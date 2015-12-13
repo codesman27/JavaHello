5b0936c2229d76c4af884a940f3eb70756b16513
codesman27 <srinu.e.archives@gmail.com>
2015-12-13 23:52:45 +0530

    newprint

--- javaFirst.java
+++ javaFirst.java
@@ -1,5 +1,7 @@
 class javaFirst {
-public static void main (String[] args){
- 	System.out.println("Hello World");
-} 
+	public static void main(String[] args) {
+		System.out.println("Hello World");
+		System.out.println("Hey");
+	}
 }
+
